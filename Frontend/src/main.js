a6431b757163167379f5a7c70cd943b903954dee
junyoung2015 <jusohn@student.42seoul.kr>
2024-05-20 11:07:35 +0000

    fix: rename Frontend to frontend #10

--- Frontend/src/main.js
+++
@@ -1,7 +0,0 @@
-import App from './app';
-import { createElement } from './lib/dom/client';
-import { router } from './lib/router';
-import { routes } from './routes';
-
-const app = document.getElementById('app');
-router(app, routes);
