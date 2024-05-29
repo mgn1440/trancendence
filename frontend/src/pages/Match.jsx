19dec5ddcbea778edcabc53308d848cb44165612
hyungjuk <hyungjuk@student.42seoul.kr>
2024-05-29 22:40:53 +0900

    feat: topNavBar와rooms 구현 #43

--- frontend/src/pages/Match.jsx
+++ frontend/src/pages/Match.jsx
@@ -1,15 +1,19 @@
 import UserList from "./components/UserList";
 import Profile from "./components/Profile";
-import MatchList from "./components/MatchList";
+import LobbyRooms from "./components/LobbyRooms";
+import TopNavBar from "./components/TopNavBar";
 
 const MatchPage = () => {
   return (
-    <div style="display: flex; flex-direction: row; ">
-      <div style="width:100%">
-        <Profile />
-        <MatchList />
+    <div>
+      <div id="top">
+        <TopNavBar />
       </div>
-      <div>
+      <div id="middle">
+        <div class="main-section">
+          <Profile />
+          <LobbyRooms />
+        </div>
         <UserList />
       </div>
     </div>
