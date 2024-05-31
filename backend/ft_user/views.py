cb5b12a797acb37ae03f57b734172d2de54b7a68
Sunghyun Ko <sunko@cx1r5s1.42seoul.kr>
2024-05-31 18:20:08 +0900

    refactor(user): space 4칸 code를 tab으로 변경함

--- backend/ft_user/views.py
+++ backend/ft_user/views.py
@@ -34,30 +34,30 @@ class OtpUpdateView(View):
 				return JsonResponse({'status': 'error', 'message': 'User not found'}, status=404)
 
 class UserDetailView(generics.RetrieveAPIView):
-    serializer_class = CustomUserSerializer
-    def get_object(self):
-        try:
-            return CustomUser.objects.get(uid=self.request.user.uid)
-        except CustomUser.DoesNotExist:
-            return None
-    def get(self, request, *args, **kwargs):
-        user = self.get_object()
-        if user is None:
-            return JsonResponse({'status_code': '400', 'message': 'User not found'}, status=400)
-        serializer = self.get_serializer(user)
-        dict_data = dict(serializer.data)
-        dict_data['status_code'] = 200
-        return JsonResponse(dict_data, status=200)
+	serializer_class = CustomUserSerializer
+	def get_object(self):
+		try:
+			return CustomUser.objects.get(uid=self.request.user.uid)
+		except CustomUser.DoesNotExist:
+			return None
+	def get(self, request, *args, **kwargs):
+		user = self.get_object()
+		if user is None:
+			return JsonResponse({'status_code': '400', 'message': 'User not found'}, status=400)
+		serializer = self.get_serializer(user)
+		dict_data = dict(serializer.data)
+		dict_data['status_code'] = 200
+		return JsonResponse(dict_data, status=200)
 
 class UserMeView(generics.RetrieveAPIView):
-    serializer_class = CustomUserSerializer
-    def get_object(self, user_id):
-        return CustomUser.objects.get(uid=user_id)
-    def get(self, request, *args, **kwargs):
-        user_id = self.kwargs.get('uid')
-        user = self.get_object(user_id)
-        serializer = self.get_serializer(user)
-        return JsonResponse(serializer.data, status=200)
+	serializer_class = CustomUserSerializer
+	def get_object(self, user_id):
+		return CustomUser.objects.get(uid=user_id)
+	def get(self, request, *args, **kwargs):
+		user_id = self.kwargs.get('uid')
+		user = self.get_object(user_id)
+		serializer = self.get_serializer(user)
+		return JsonResponse(serializer.data, status=200)
 
 class UserWinUpdateView(View):
 	# permission_classes = [IsAuthenticated]
@@ -96,15 +96,15 @@ class UserLoseUpdateView(View):
 			return JsonResponse({'status': 'error', 'message': 'User not found'}, status=404)
 
 class SingleGameRecordListView(ListAPIView):
-    serializer_class = SingleGameRecordSerializer
+	serializer_class = SingleGameRecordSerializer
 
-    def get_queryset(self):
-        user_id = self.kwargs['user_id']
-        try:
-            user = CustomUser.objects.get(uid=user_id)
-            return SingleGameRecord.objects.filter(user=user)
-        except CustomUser.DoesNotExist:
-            raise NotFound("User does not exist")
+	def get_queryset(self):
+		user_id = self.kwargs['user_id']
+		try:
+			user = CustomUser.objects.get(uid=user_id)
+			return SingleGameRecord.objects.filter(user=user)
+		except CustomUser.DoesNotExist:
+			raise NotFound("User does not exist")
 
 class FriendView(View):
 	def get(self, request):
