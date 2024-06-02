27eba1084c212911ae8a13ba90c87b3a586ebedd
jusohn Eddie Sohn <jusohn@student.42seoul.kr>
2024-06-02 20:20:32 +0900

    fix: setCookie 의 value 가 없을 때 기본값 추가

--- frontend/src/api/cookie.js
+++ frontend/src/api/cookie.js
@@ -3,7 +3,7 @@ const getCookie = (key) => {
   return cookie ? cookie.split("=")[1] : null;
 };
 
-const setCookie = (key, value, days = 1) => {
+const setCookie = (key, value = "", days = 1) => {
   const date = new Date();
   date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
   document.cookie = `${key}=${value}; expires=${date.toUTCString()}; path=/`;
