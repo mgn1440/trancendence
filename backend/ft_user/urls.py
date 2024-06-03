61b9962a5642fd6fd441c425d09c98d6e1fe3e12
Sunghyun Ko <sunko@cx1r5s1.42seoul.kr>
2024-06-03 20:39:08 +0900

    feat(user): api/user/friend/{user_id} delete method 추가

--- backend/ft_user/urls.py
+++ backend/ft_user/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import render
-from .views import OtpUpdateView, UserDetailView, UserWinUpdateView, UserLoseUpdateView, FriendView, logout, test_friend, SingleGameRecordListView
+from .views import OtpUpdateView, UserDetailView, UserWinUpdateView, UserLoseUpdateView, FriendView, logout, test_friend, SingleGameRecordListView, FriendDetailView
 
 urlpatterns = [
 	path('otp/', OtpUpdateView.as_view(), name='otp_update'),
@@ -9,17 +9,9 @@ urlpatterns = [
  	path('win/', UserWinUpdateView.as_view(), name='user_win_update'),
     path('lose/', UserLoseUpdateView.as_view(), name='user_lose_update'),
 	path('friend/', FriendView.as_view(), name='friend'),
-	#path('friend/<int:friend_id>/', FriendView.as_view(), name='friend'),
+	path('friend/<int:friend_id>/', FriendDetailView.as_view(), name='friend_detail'),
 	path('test_friend/', test_friend, name='test_friend'),
 	path('logout/', logout, name='logout'),
-  path('<int:uid>/', UserDetailView.as_view(), name='user_detail'),
+	path('<int:uid>/', UserDetailView.as_view(), name='user_detail'),
 	path('<int:user_id>/game-records/', SingleGameRecordListView.as_view(), name='game_record'),
-]
-
-urlpatterns = [
-	path('otp/', OtpUpdateView.as_view(), name='otp_update'),
-	path('me/', UserMeView.as_view(), name='me'),
-	path('win/', UserWinUpdateView.as_view(), name='user_win_update'),
-	path('lose/', UserLoseUpdateView.as_view(), name='user_lose_update'),
-	
 ]
