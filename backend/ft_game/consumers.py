f68da01468743650abbf637fb16d7d7920ae74b3
Suro Kim <surkim@cx1r2s1.42seoul.kr>
2024-06-14 14:12:15 +0900

    fix(game): game room id를 host에서 숫자로 변경 #161

--- backend/ft_game/consumers.py
+++ backend/ft_game/consumers.py
@@ -13,8 +13,9 @@ from asgiref.sync import sync_to_async
 
 class GameConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.host_username = self.scope['url_route']['kwargs']['host_username']
-        self.room_group_name = self.host_username
+        self.room_id_str = self.scope['url_route']['kwargs']['room_id']
+        self.room_group_name = self.room_id_str
+        self.room_id = int(self.room_id_str)
         self.status = 'waiting'
 
         await self.channel_layer.group_add(
@@ -23,29 +24,29 @@ class GameConsumer(AsyncWebsocketConsumer):
         )
         await self.accept()
 
-        if self.host_username not in LobbyConsumer.rooms:
+        if self.room_id not in LobbyConsumer.rooms:
             await self.send_error_message('Room does not exist')
             await self.close()
             return
 
-        if LobbyConsumer.rooms[self.host_username]['status'] != 'game':
+        if LobbyConsumer.rooms[self.room_id]['status'] != 'game':
             await self.send_error_message('Room is not in game status')
             await self.close()
             return
 
-        if self.scope['user'].username not in LobbyConsumer.rooms[self.host_username]['in_game_players']:
+        if self.scope['user'].username not in LobbyConsumer.rooms[self.room_id]['in_game_players']:
             await self.send_error_message('You are not in game')
             await self.close()
             return
         
-        if LobbyConsumer.rooms[self.host_username]['mode'] != 'matchmaking' and \
-            LobbyConsumer.rooms[self.host_username]['mode'] != 2:
+        if LobbyConsumer.rooms[self.room_id]['mode'] != 'matchmaking' and \
+            LobbyConsumer.rooms[self.room_id]['mode'] != 2:
             await self.send_error_message('Room is not in matchmaking mode')
             await self.close()
             return
 
-        if 'game' not in LobbyConsumer.rooms[self.host_username]:
-            LobbyConsumer.rooms[self.host_username]['game'] = {
+        if 'game' not in LobbyConsumer.rooms[self.room_id]:
+            LobbyConsumer.rooms[self.room_id]['game'] = {
                 'ball': {'x': 600, 'y': 450, 'radius': 10, 'speedX': 10, 'speedY': 10},
                 'player_bar': {'left': 360, 'right': 360},
                 'scores': {'left': 0, 'right': 0},
@@ -54,21 +55,21 @@ class GameConsumer(AsyncWebsocketConsumer):
                 'bar_move': {'left': 0, 'right': 0},
             }
 
-        LobbyConsumer.rooms[self.host_username]['game']['players'].append(self.scope['user'].username)
-        if len(LobbyConsumer.rooms[self.host_username]['game']['players']) == 2:
-            LobbyConsumer.rooms[self.host_username]['game']['roles'] = {
-                'left': LobbyConsumer.rooms[self.host_username]['in_game_players'][0],
-                'right': LobbyConsumer.rooms[self.host_username]['in_game_players'][1],
+        LobbyConsumer.rooms[self.room_id]['game']['players'].append(self.scope['user'].username)
+        if len(LobbyConsumer.rooms[self.room_id]['game']['players']) == 2:
+            LobbyConsumer.rooms[self.room_id]['game']['roles'] = {
+                'left': LobbyConsumer.rooms[self.room_id]['in_game_players'][0],
+                'right': LobbyConsumer.rooms[self.room_id]['in_game_players'][1],
             }
             await self.channel_layer.group_send(
                 self.room_group_name,
                 {
                     'type': 'game_start',
-                    'game': LobbyConsumer.rooms[self.host_username]['game']
+                    'game': LobbyConsumer.rooms[self.room_id]['game']
                 }
             )
-            LobbyConsumer.rooms[self.host_username]['status'] = 'playing'
-        self.game = LobbyConsumer.rooms[self.host_username]['game']
+            LobbyConsumer.rooms[self.room_id]['status'] = 'playing'
+        self.game = LobbyConsumer.rooms[self.room_id]['game']
 
     async def send_error_message(self, message):
         await self.send(text_data=json.dumps({
@@ -78,7 +79,7 @@ class GameConsumer(AsyncWebsocketConsumer):
         self.status = 'connect error'
 
     async def disconnect(self, close_code):
-        if self.host_username not in LobbyConsumer.rooms:
+        if self.room_id not in LobbyConsumer.rooms:
             await self.channel_layer.group_discard(
                 self.room_group_name,
                 self.channel_name
@@ -101,16 +102,16 @@ class GameConsumer(AsyncWebsocketConsumer):
                     'message': 'user disconnected'
                 }
             )
-            LobbyConsumer.rooms[self.host_username]['game']['players'].remove(self.scope['user'].username)
+            LobbyConsumer.rooms[self.room_id]['game']['players'].remove(self.scope['user'].username)
 
-            if len(LobbyConsumer.rooms[self.host_username]['game']['players']) == 1:
-                winner_name = LobbyConsumer.rooms[self.host_username]['game']['players'][0]
+            if len(LobbyConsumer.rooms[self.room_id]['game']['players']) == 1:
+                winner_name = LobbyConsumer.rooms[self.room_id]['game']['players'][0]
                 loser_name = self.scope['user'].username
                 game_data = await GameConsumer.get_game_data(winner_name, loser_name, 5, 0)
                 await GameConsumer.create_game_records(game_data)
-            elif len(LobbyConsumer.rooms[self.host_username]['game']['players']) == 0:
+            elif len(LobbyConsumer.rooms[self.room_id]['game']['players']) == 0:
 
-                del LobbyConsumer.rooms[self.host_username]
+                del LobbyConsumer.rooms[self.room_id]
             await self.update_room_list()
 
         await self.channel_layer.group_discard(
@@ -122,7 +123,7 @@ class GameConsumer(AsyncWebsocketConsumer):
         data = json.loads(text_data)
         print(data)
         if data['type'] == 'start_game':
-            if self.host_username in LobbyConsumer.rooms and len(LobbyConsumer.rooms[self.host_username]['game']['players']) != 2:
+            if self.room_id in LobbyConsumer.rooms and len(LobbyConsumer.rooms[self.room_id]['game']['players']) != 2:
                 await self.channel_layer.group_send(
                     self.room_group_name,
                     {
@@ -132,27 +133,21 @@ class GameConsumer(AsyncWebsocketConsumer):
                 )
                 return
             self.status = 'playing'
-            if self.host_username in LobbyConsumer.rooms and self.scope['user'].username == LobbyConsumer.rooms[self.host_username]['game']['roles']['left']:
+            if self.room_id in LobbyConsumer.rooms and self.scope['user'].username == LobbyConsumer.rooms[self.room_id]['game']['roles']['left']:
                 asyncio.create_task(self.start_ball_movement())
         elif data['type'] == 'move_bar':
-            # asyncio.create_task(self.update_bar_position(data['direction'], data['role']))
             self.update_bar_position(data['direction'], data['role'])
         elif data['type'] == 'stop_bar':
-            print('stop')
             self.game['bar_move'][data['role']] = 0
-        # elif data['type'] == 'error':
-        #     await self.send_error_message(data['message'])
-        #     del LobbyConsumer.rooms[self.host_username]
-        #     await self.update_room_list()
 
     async def start_ball_movement(self):
-        while self.status == 'playing' and self.host_username in LobbyConsumer.rooms and len(LobbyConsumer.rooms[self.host_username]['game']['players']) == 2:
+        while self.status == 'playing' and self.room_id in LobbyConsumer.rooms and len(LobbyConsumer.rooms[self.room_id]['game']['players']) == 2:
             self.update_ball_position()
             await self.channel_layer.group_send(
                 self.room_group_name,
                 {
                     'type': 'update_game',
-                    'game': LobbyConsumer.rooms[self.host_username]['game']
+                    'game': LobbyConsumer.rooms[self.room_id]['game']
                 }
             )
             await asyncio.sleep(0.03)
@@ -209,13 +204,13 @@ class GameConsumer(AsyncWebsocketConsumer):
 
     async def game_end(self, winner, loser):
         self.status = 'game_over'
-        LobbyConsumer.rooms[self.host_username]['status'] = 'room'
-        LobbyConsumer.rooms[self.host_username]['in_game_players'] = []
-        winner_username = LobbyConsumer.rooms[self.host_username]['game']['roles'][winner]
-        loser_username = LobbyConsumer.rooms[self.host_username]['game']['roles'][loser]
-        del LobbyConsumer.rooms[self.host_username]['game']
-        if LobbyConsumer.rooms[self.host_username]['mode'] == 'matchmaking':
-            del LobbyConsumer.rooms[self.host_username]
+        LobbyConsumer.rooms[self.room_id]['status'] = 'room'
+        LobbyConsumer.rooms[self.room_id]['in_game_players'] = []
+        winner_username = LobbyConsumer.rooms[self.room_id]['game']['roles'][winner]
+        loser_username = LobbyConsumer.rooms[self.room_id]['game']['roles'][loser]
+        del LobbyConsumer.rooms[self.room_id]['game']
+        if LobbyConsumer.rooms[self.room_id]['mode'] == 'matchmaking':
+            del LobbyConsumer.rooms[self.room_id]
         winner_score = self.game['scores'][winner]
         loser_score = self.game['scores'][loser]
         game_data = await GameConsumer.get_game_data(winner_username, loser_username, winner_score, loser_score)
@@ -266,7 +261,7 @@ class GameConsumer(AsyncWebsocketConsumer):
             'type': 'game_over',
             'winner': event['winner'],
             'loser': event['loser'],
-            'host_username': self.host_username
+            'room_id': self.room_id
         }))
 
     async def error(self, event):
--- backend/ft_game/views.py
+++ backend/ft_game/views.py
@@ -4,8 +4,8 @@ def game_room(request, room_number):
     return render(request, 'game_room.html', {"room_number": room_number})
 
 
-def game(request, host_username):
-	return render(request, 'game.html', {"host_username": host_username})
+def game(request, room_id):
+	return render(request, 'game.html', {"room_id": room_id})
 
 def tournament_game(request, host_username):
 	return render(request, 'tournament_game.html', {"host_username": host_username})
