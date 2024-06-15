f47b973897994dcf788b455323060c0d27c03b7b
hyungjuk <hyungjuk@student.42seoul.kr>
2024-06-15 12:05:35 +0900

    fix: parcel tester #179

---
+++ frontend/parcel.js
@@ -0,0 +1,7 @@
+const parcelElement = () => {
+  console.log("parcelElement");
+  const parcelElement = document.getElementById("app");
+  parcelElement.innerHTML = "Parcel Element";
+};
+
+parcelElement();
