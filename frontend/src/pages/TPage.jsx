882329ca25625a39e66d720b075a14016e2c2076
mgn1440 <mgn1440@gmail.com>
2024-06-20 21:19:54 +0900

    feat:  토너먼트 프론트엔드와 연결 ##209

--- frontend/src/pages/TPage.jsx
+++ frontend/src/pages/TPage.jsx
@@ -1,14 +1,10 @@
+import Bracket from "./components/Bracket";
+import GamePage from "./Tournament";
+
 const TestPage = () => {
   return (
     <div class="test-game-main">
-      <div id="test-game"></div>
-      <div class="test-game-info">
-        <p class="user1">test1</p>
-        <p class="user2">test2</p>
-        <h6 class="user1">gameStat[0].left</h6>
-        <h6 class="user2">gameStat[0].right</h6>
-        <h1>3</h1>
-      </div>
+      <GamePage />
     </div>
   );
 };
