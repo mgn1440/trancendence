8c3cc136591d29b7dd095cab7e0dff4dd6cb4a11
Sunghyun Ko <sunko@c1r8s3.42seoul.kr>
2024-06-24 23:39:58 +0900

    fix: create cert container/#268

--- cert/generate-certificate.sh
+++ cert/generate-certificate.sh
@@ -1,42 +1,6 @@
 #!/bin/sh
 
 # Generate a self-signed certificate for the server
-# mkdir -p /etc/ssl/certs
-
-# echo "[SAN]\nsubjectAltName=DNS:prometheus" > /tmp/san_config.cnf \
-#     && cat /etc/ssl/openssl.cnf /tmp/san_config.cnf > /tmp/openssl.cnf \
-#     && openssl req -x509 -newkey rsa:2048 -nodes \
-#        -keyout /etc/ssl/certs/prometheus-selfsigned.key \
-#        -out /etc/ssl/certs/prometheus-selfsigned.crt \
-#        -subj "/C=KR/ST=GEYOUNG-GI-DO/L=SEOUL/O=42SEOUL/OU=CARDET/CN=prometheus" \
-#        -reqexts SAN \
-#        -config /tmp/openssl.cnf \
-#        -extensions SAN
-
-
-# echo "[SAN]\nsubjectAltName=DNS:node-exporter" > /tmp/san_config.cnf \
-#     && cat /etc/ssl/openssl.cnf /tmp/san_config.cnf > /tmp/openssl.cnf \
-#     && openssl req -x509 -newkey rsa:2048 -nodes \
-#        -keyout /etc/ssl/certs/node-exporter-selfsigned.key \
-#        -out /etc/ssl/certs/node-exporter-selfsigned.crt \
-#        -subj "/C=KR/ST=GEYOUNG-GI-DO/L=SEOUL/O=42SEOUL/OU=CARDET/CN=node-exporter" \
-#        -reqexts SAN \
-#        -config /tmp/openssl.cnf \
-#        -extensions SAN
-
-# echo "[SAN]\nsubjectAltName=DNS:alertmanager" > /tmp/san_config.cnf \
-#     && cat /etc/ssl/openssl.cnf /tmp/san_config.cnf > /tmp/openssl.cnf \
-#     && openssl req -x509 -newkey rsa:2048 -nodes \
-#        -keyout /etc/ssl/certs/alertmanager-selfsigned.key \
-#        -out /etc/ssl/certs/alertmanager-selfsigned.crt \
-#        -subj "/C=KR/ST=GEYOUNG-GI-DO/L=SEOUL/O=42SEOUL/OU=CARDET/CN=alertmanager" \
-#        -reqexts SAN \
-#        -config /tmp/openssl.cnf \
-#        -extensions SAN
-
-#!/bin/sh
-
-# Generate a self-signed certificate for the server
 mkdir -p /etc/ssl/certs
 
 generate_cert() {
@@ -79,9 +43,5 @@ DNS.1 = $service_name
 generate_cert "prometheus" "prometheus"
 generate_cert "node-exporter" "node-exporter"
 generate_cert "alertmanager" "alertmanager"
-
-echo "All certificates generated successfully"
-
-
 
 echo "Certificates generated successfully"
